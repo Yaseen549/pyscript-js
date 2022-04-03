1c33543c6bd2b27ff94b5356b62200a5006a7bf7
Yaseen <fantasticyaseenshariff@gmail.com>
2022-04-03 20:25:26 +0530

    first commit

---
+++ dist/js/pyscript.js
@@ -0,0 +1,28 @@
+// alert("Hello world!")
+
+// Input Funciton
+function input(text) {
+  /** funciton input as in python **/
+    inputText = prompt(text);
+    return inputText;
+}
+
+// Print Function
+function print(params) {
+  /** funciton print as in python **/
+    console.log(params);
+}
+
+// Number Function
+function number(params) {
+  /** funciton int as in python **/
+    return parseInt(params);
+}
+
+// alert Function
+function popup(params){
+  alert(params);
+}
+
+
+// export alert("hi");
---
+++ file.js
@@ -0,0 +1,8 @@
+// import * from '/dist/js/pyscript.js'
+//
+// alert("HI")
+// 📁 main.js
+import {sayHi, sayBye} from './say.js';
+
+sayHi('John'); // Hello, John!
+sayBye('John'); // Bye, John!
